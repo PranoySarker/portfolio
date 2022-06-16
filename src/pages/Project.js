4fe2f0a81577ba55642c2fde45492292db38adee
PranoySarker <pranoysarker01@gmail.com>
2022-06-16 13:04:13 +0600

    some changes

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,14 +5,14 @@ const Project = ({ project }) => {
     const { _id, name, image1 } = project;
 
     return (
-        <div>
-            <div class="card w-96 bg-base-100 shadow-xl">
+        <div className='mt-5'>
+            <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={image1} alt="project" style={{ 'height': '500px', 'width': '300px' }} /></figure>
-                <div class="card-body">
-                    <h2 class="card-title">{name}</h2>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
                     <p>You can see the details of projects</p>
-                    <div class="card-actions ">
-                        <Link to={`/details/${_id}`}> <button class="btn btn-secondary text-white">Click Here..</button></Link>
+                    <div className="card-actions ">
+                        <Link to={`/details/${_id}`}> <button className="btn btn-secondary text-white">Click Here..</button></Link>
                     </div>
                 </div>
             </div>
